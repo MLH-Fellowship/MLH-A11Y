a73ce683537f3131e0950ae3fdcf32d13d843cd0
Ashley Ye <ashleyye@berkeley.edu>
2021-08-10 14:58:54 -0700

    finished framework

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Unit1 from './unit1';
+import Unit2 from './unit2';
+import Unit3 from './unit3';
 import HomeMarkdown from './home';
 import UserMarkdown from './userprofile';
+import LogIn from './login';
+import Register from './register'
 import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
@@ -23,13 +27,14 @@ function App() {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/user">User</Nav.Link>
+                <Nav.Link href="/user">Profile</Nav.Link>
                 <NavDropdown title="Units" id="basic-nav-dropdown">
                   <NavDropdown.Item href="/unit1">Unit 1</NavDropdown.Item>
                   <NavDropdown.Item href="/unit2">Unit 2</NavDropdown.Item>
                   <NavDropdown.Item href="/unit3">Unit 3</NavDropdown.Item>
                 </NavDropdown>
-                
+                <Nav.Link href="/login">Log In</Nav.Link>
+                <Nav.Link href="/register">Register</Nav.Link>
               </Nav>
             </Navbar.Collapse>
             
@@ -40,8 +45,20 @@ function App() {
             <Route path="/unit1">
               <Unit1 unit={"1"}/>
             </Route>
+            <Route path="/unit2">
+              <Unit2 unit={"2"}/>
+            </Route>
+            <Route path="/unit3">
+              <Unit3 unit={"3"}/>
+            </Route>
             <Route path="/user">
               <UserMarkdown />
+            </Route>
+            <Route path="/login">
+              <LogIn />
+            </Route>
+            <Route path="/register">
+              <Register />
             </Route>
             <Route path="/">
               <HomeMarkdown />
